2fb80f05805fa84b5355a8e85bcefc50befd6275
Daniel <dpadayao40@gmail.com>
2021-05-18 05:42:24 -0400

    remove debugging guideline

--- pages/pokemon.js
+++ pages/pokemon.js
@@ -47,26 +47,27 @@ export default function pokemon({ pokeman }) {
                 <Heading as={motion.h1} mb={2} fontSize='4xl' textTransform='capitalize' color="teal.400"
                     layoutId={name}> { name }</Heading>
                 <hr />
+                <br />
                 
                 <Flex flexDirection='column' width='full'>
-                    <Heading mt={10}  fontSize='2xl' >Status</Heading> 
-                    <Text px={4} mt={3}><span>Weight: </span> {weight} KG</Text>
+                    <Heading mt={10}  fontSize='xl' >Status</Heading> 
+                    <Text px={4} mt={3}><span>Weight: </span> {weight} G</Text>
                     <Text px={4} ><span>Height: </span> {height} CM</Text>
                 </Flex>
                 <Flex flexDirection='column' width='full'>
-                 <Heading mt={15}  fontSize='2xl' >Ability</Heading> 
+                 <Heading mt={18} color='blue.400'  fontSize='xl' >Ability</Heading> 
                     <HStack my={3} spacing={3}>
                         {abilities.map(( mainAbility ) => (
                             // const { ability: {name: abiName}} = mainAbility;
                             <Tag key={mainAbility.ability.name}
-                            size="lg" colorScheme='yellow' borderRadius="full" textTransform='capitalize'
+                            size="lg" colorScheme='facebook' borderRadius="full" textTransform='capitalize'
                             px={4}
                             >{mainAbility.ability.name}</Tag>
                            
                         ))}
                     </HStack>
 
-                    <Heading mt={15}  fontSize='2xl' >Types</Heading> 
+                    <Heading mt={18}  color='green.400'  fontSize='xl' >Types</Heading> 
                     <HStack my={3} spacing={3}>
                         {types.map((type, index) => (
 
@@ -76,11 +77,11 @@ export default function pokemon({ pokeman }) {
                                 >{type.type.name}</Tag>
                         ))}
                     </HStack>
-                    <Heading mt={15}  fontSize='2xl' >Special Moves</Heading> 
-                    <Flex justifyContent='normal' w='auto' bg='green' flexWrap='wrap' px={2} m={6}>
+                    <Heading mt={18} color='purple.400' fontSize='xl' >Special Moves</Heading> 
+                    <Flex justifyContent='normal' w='auto' flexWrap='wrap' px={2} m={6}>
                         {moves.map(( specialMoves) => (
                            <Tag key={specialMoves.move.name}
-                           size='sm' colorScheme='blue' height={8} w='fit-content' borderRadius="full" textTransform='capitalize'
+                           size='sm' colorScheme='purple' px={3} height={8} w='fit-content' borderRadius="full" textTransform='capitalize'
                            fontSize={12}
                            m={2}
                            >{specialMoves.move.name}</Tag>
